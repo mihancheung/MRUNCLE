b2d0f600ada9c21223b83f704b0013b5a6db24dd
mihancheung <mihancheung@gmail.com>
2020-04-27 00:55:03 +0800

    feat: 文章渲染逻辑

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 App({
-  onLaunch: function () {
+  onLaunch () {
     wx.cloud.init({
       env: 'dev-c8vh9'
     })
@@ -7,5 +7,23 @@ App({
 
   postAvata: 'cloud://release-vp8ak.7265-release-vp8ak-1301890037/me.jpeg',
   postAuthor: 'MRUNCLE',
-  towxml:require('/towxml/index')
+  towxml:require('/towxml/index'),
+
+  wxRequire (opt) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        method: 'GET',
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          resolve(res);
+        },
+        fail: function (error) {
+          reject(error);
+        },
+        ...opt,
+      });
+    })
+  }
 })
--- pages/post/post.js
+++ pages/post/post.js
@@ -1,15 +1,94 @@
-const app = getApp()
+import { formatDate } from '../../utils/index';
+
+const app = getApp();
+const db = wx.cloud.database();
+const post = db.collection('post');
 
 Page({
   data: {
     info: {
-      title: "101%还原",
-      tags: ['模块化', 'CSS'],
+      title: "",
+      tags: [],
       avata: app.postAvata,
       author: app.postAuthor,
-      date: '2020-02-02',
-      poster: 'cloud://release-vp8ak.7265-release-vp8ak-1301890037/post/101percent/img/banner.png',
+      date: '',
+      poster: '',
+      mdFileId: '',
     },
-    md: {}
+    md: '',
+    isError: false,
+    isLoading: true
+  },
+
+  onLoad () {
+    this.init()
+  },
+
+  init () {
+    this.getPageInfo()
+  },
+
+  mdToWxml (mdFile) {
+    if (!mdFile) return;
+
+    const md = app.towxml(mdFile, 'markdown',{
+      theme:'light',
+    });
+
+    this.setData({
+      md
+    });
+  },
+
+  handleNextData (data) {
+    const { date: dataDate, avata: dataAvata, author: dataAuthor } = data;
+    const avata = dataAvata || app.postAvata;
+    const author = dataAuthor || app.postAuthor;
+    const date = formatDate(+new Date(dataDate));
+
+    this.setData({
+      info: {
+        ...data,
+        avata,
+        author,
+        date,
+      }
+    });
+  },
+
+  async getPageInfo () {
+    const postId = '2a625d2b5ea3c6f5001fa82801ea0590';
+    const res = await post.doc(postId).get();
+    this.setData({
+      isLoading: false
+    });
+
+    if (!res || !res.data) {
+      this.setData({
+        isError: true
+      });
+      return;
+    }
+
+    this.handleNextData(res.data);
+    this.getPageMdFile(res.data.mdFileId);
+  },
+
+  async getPageMdFile (mdFileId) {
+    const res = await wx.cloud.getTempFileURL({
+      fileList: [mdFileId],
+    });
+
+    if (!res || !res.fileList) {
+      return;
+    }
+
+    const url = res.fileList[0] && res.fileList[0].tempFileURL
+    const mdFileRes = await app.wxRequire({
+      url
+    });
+
+    if (!mdFileRes || !mdFileRes.data) return
+    this.mdToWxml(mdFileRes.data);
   }
 })
